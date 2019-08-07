da9111397e5a54b2472901c8dbd9df31907fb061
Marcel Maltry <mmaltry+github@mailbox.org>
2019-08-07 14:20:27 +0200

    Added alias for changing file and folder ownership to emby.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 # start and stop postgres server
 alias pg_start="pg_ctl start -D /usr/local/var/postgres"
 alias pg_stop="pg_ctl stop -D /usr/local/var/postgres"
+
+# change ownership to emby
+alias chown_emby="sudo chown -R emby:emby"
