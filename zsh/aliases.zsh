1448c033e7c3d6a11e8928742a11028e4494a9ca
Marcel Maltry <mmaltry+github@mailbox.org>
2019-09-02 11:11:43 +0200

    Added colorized tree alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
+# colorize output of `tree`
+alias tree="tree -C"
+
 # start and stop postgres server
 alias pg_start="pg_ctl start -D /usr/local/var/postgres"
 alias pg_stop="pg_ctl stop -D /usr/local/var/postgres"
