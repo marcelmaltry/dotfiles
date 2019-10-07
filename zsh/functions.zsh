837b752019abc4df995f64474a9eaf55780321f0
Marcel Maltry <mmaltry+github@mailbox.org>
2019-10-07 14:42:42 +0200

    Added function to compress pdf files based on ghostscript.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,12 +2,25 @@
 
 # Create a new directory and enter it
 mkcd() {
-	mkdir -p "$@" && cd "$@";
+    mkdir -p "$@" && cd "$@";
 }
 
 # Download a list of urls
 aria() {
-	for x in "$@"; do
-		aria2c $x;
-	done
+    for x in "$@"; do
+        aria2c $x;
+    done
+}
+
+# Compress pdf with ghostscript
+compresspdf () {
+    OLDNAME="$1"
+    if [ ${OLDNAME: -4}==".pdf" ]
+    then
+        NEWNAME="${OLDNAME%%.*}-compressed.pdf"
+        gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$NEWNAME" "$OLDNAME"
+        echo "Compressed pdf saved to $NEWNAME."
+    else
+        echo "File is not a .pdf file."
+    fi
 }
