10d8ec2d3ce543b1fed2921bf0bba4b21e7cc19f
Marcel Maltry <mmaltry+github@mailbox.org>
2019-10-24 11:28:35 +0200

    Added function `termcolors` to display terminal colors.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -24,3 +24,17 @@ compresspdf () {
         echo "File is not a .pdf file."
     fi
 }
+
+# Show all available terminal colors
+termcolor()
+{
+    for x in {0..8}; do 
+        for i in {30..37}; do 
+            for a in {40..47}; do 
+                echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+            done
+            echo
+        done
+    done
+    echo ""
+}
