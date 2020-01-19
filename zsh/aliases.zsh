6d0111285c677ec41a6e6908ec19fb8331832157
Marcel Maltry <mmaltry+github@mailbox.org>
2020-01-19 17:20:25 +0100

    Remove white space in aliases.zsh.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias tree="tree -C"
 alias pg_start="pg_ctl start -D /usr/local/var/postgres"
 alias pg_stop="pg_ctl stop -D /usr/local/var/postgres"
 
-# Emby helpers
+# Emby helpers 
 alias embyown="sudo chown -R emby:emby ."
 
 # neovim
