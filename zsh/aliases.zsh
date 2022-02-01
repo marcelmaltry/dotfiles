52ee945b093da3c41fb049a5b1e8fc139c15e1ad
Marcel Maltry <mmaltry+github@mailbox.org>
2022-02-01 12:21:42 +0100

    Remove system-specific aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,6 @@
 # colorize output of `tree`
 alias tree="tree -C"
 
-# start and stop postgres server
-alias pg_start="pg_ctl -D /usr/local/var/postgres start"
-alias pg_stop="pg_ctl -D /usr/local/var/postgres stop"
-
-# change ownership to emby
-alias chown_emby="sudo chown -R emby:emby"
-
 # neovim
 alias n='nvim'
 
