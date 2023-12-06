e0500291990e6275245bde0700b3b0f7abadb0a5
Marcel Maltry <mmaltry+github@mailbox.org>
2023-12-06 21:15:45 +0100

    [wezterm] Add initial config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,69 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- colors and appearance
+config.color_scheme = 'Material (base16)'
+color_bg = '#263238'
+color_fg = '#eeffff'
+color_sel = '#2c3b41'
+-- tab bar
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_frame = {
+    font = wezterm.font({
+        family = 'MesloLGM Nerd Font',
+        harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+    }),
+    font_size = 12.0,
+    active_titlebar_bg = color_bg,
+    inactive_titlebar_bg = color_bg,
+}
+config.colors = {
+    tab_bar = {
+        inactive_tab_edge = color_bg,
+        active_tab = {
+            bg_color = color_sel,
+            fg_color = color_fg,
+        },
+        inactive_tab = {
+            bg_color = color_bg,
+            fg_color = color_fg,
+        },
+        inactive_tab_hover = {
+            bg_color = color_sel,
+            fg_color = color_fg,
+        },
+        new_tab = {
+            bg_color = color_bg,
+            fg_color = color_fg,
+        },
+        new_tab_hover = {
+            bg_color = color_sel,
+            fg_color = color_fg,
+        },
+    },
+}
+
+-- cursor
+config.default_cursor_style = 'BlinkingBar'
+
+-- font
+config.font = wezterm.font({
+    family = 'MesloLGM Nerd Font',
+    harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+})
+
+-- key binding
+config.keys = {
+    -- OPT + Left -> backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- OPT + Right -> forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
+
+return config
